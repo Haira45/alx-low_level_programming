923d77568e335f6e2e2af619b87fa140c0414546
Haira45 <humuhairayahaya100@gmail.com>
2023-07-28 02:15:05 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,19 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int count_of_bytes;
+	int j;
 
-	for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0'; count_of_bytes++)
-		dest[count_of_bytes] = src[count_of_bytes];
-	for (; count_of_bytes < n; count_of_bytes++)
-		dest[count_of_bytes] = '\0';
+	j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 	return (dest);
 }
